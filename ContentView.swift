5ba0f1e6bcd05fa343c0550bf3e404b6f990c76a
kavit18 <kavit1808@gmail.com>
2020-03-23 19:20:09 -0700

    Question 1

---
+++ ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Mid_term
+//
+//  Created by Kavit Sanghavi on 23/03/20.
+//  Copyright © 2020 Kavit Sanghavi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("I am an iOS Developer!")
+        .padding()
+        .background(Color.red)
+        .padding()
+        .background(Color.blue)
+        .padding()
+        .padding()
+        .padding()
+        .background(Color.green)
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
